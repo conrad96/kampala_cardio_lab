65f781c6394f78fd170a18dd24a485fd709ac7a9
conrad96 <conrad.mugisha@gmail.com>
2020-01-09 14:27:36 +0300

    code for dynamic header

---
+++ application/views/shared/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Kampala Cardio Lab - <?php echo strtoupper(!empty($page)? $page : ''); ?></title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="<?php echo base_url(); ?>assets/img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/responsive/responsive.css" rel="stylesheet">
+
+</head>
