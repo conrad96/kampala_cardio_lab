05ab21e1fe3cb5149c489de9227f1063f28cdd89
conrad96 <conrad.mugisha@gmail.com>
2020-01-09 20:53:14 +0300

    content change on cards below top header section

--- application/views/index.php
+++ application/views/index.php
@@ -47,19 +47,19 @@
                             <div class="col-12 col-md-4">
                                 <div class="single-top-feature">
                                     <h5><i class="fa fa-clock-o" aria-hidden="true"></i> Expertise</h5>                                    
-                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                                    <p>We pride ourselves in delivering the best cardiography in the country with State of the Art Equipment
                                 </div>
                             </div>
                             <div class="col-12 col-md-4">
                                 <div class="single-top-feature">
                                     <h5><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> Reliability</h5>
-                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                                    <p>We keep in touch with our clients and monitor their recovery 
                                 </div>
                             </div>
                             <div class="col-12 col-md-4">
                                 <div class="single-top-feature">
-                                    <h5><i class="fa fa-diamond" aria-hidden="true"></i> Quality</h5>
-                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                                    <h5><i class="fa fa-diamond" aria-hidden="true"></i> Quality Treatment</h5>
+                                    <p>Experienced team of doctors and nurses to treat you
                                 </div>
                             </div>
                         </div>
