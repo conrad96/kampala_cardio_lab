6f60ecd12af2d117cf6c9822ab85f1f6ea072e52
conrad96 <conrad.mugisha@gmail.com>
2020-01-09 14:35:03 +0300

    changed default route to Home

---
+++ application/views/shared/scripts.php
@@ -0,0 +1,10 @@
+<!-- jQuery-2.2.4 js -->
+<script src="<?php echo base_url(); ?>assets/js/jquery/jquery-2.2.4.min.js"></script>
+<!-- Popper js -->
+<script src="<?php echo base_url(); ?>assets/js/bootstrap/popper.min.js"></script>
+<!-- Bootstrap-4 js -->
+<script src="<?php echo base_url(); ?>assets/js/bootstrap/bootstrap.min.js"></script>
+<!-- All Plugins js -->
+<script src="<?php echo base_url(); ?>assets/js/others/plugins.js"></script>
+<!-- Active JS -->
+<script src="<?php echo base_url(); ?>assets/js/active.js"></script>
