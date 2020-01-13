223a2505cc431fb568401a8d1e66be696fd0e5d4
conrad96 <conrad.mugisha@gmail.com>
2020-01-13 23:25:33 +0300

    added images for fun facts slideshow section

---
+++ application/views/shared/fun_facts.php
@@ -0,0 +1,109 @@
+<section class="fancy-testimonials-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="testimonials-slides owl-carousel">
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                                <img src="<?php  echo base_url(); ?>assets/img/pictures/fist.jpg" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The average heart is the size of a fist in an adult.</h5>
+                                <h6>Fun facts <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/heart.gif" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>Your heart will beat about 115,000 times each day.</h5>
+                                <h6>Fun facts - <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/giraffe.jpg" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The giraffe has a lopsided heart, with their left ventricle being thicker than the right. This is because the left side has to get blood up the giraffe’s long neck to reach their brain.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/heart_symbol.gif" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The iconic heart shape as a symbol of love is traditionally thought to come from the silphium plant, which was used as an ancient form of birth control.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/laughing_man.jpg" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>Laughing is good for your heart. It reduces stress and gives a boost to your immune system.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/heart_beat.gif" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The beating sound of your heart is caused by the valves of the heart opening and closing.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/human_heart.jpg" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The human heart weighs less than 1 pound. However, a man’s heart, on average, is 2 ounces heavier than a woman’s heart.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                            <img src="<?php  echo base_url(); ?>assets/img/pictures/shrew.jpg" class="facts-imgs" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>The American pygmy shrew is the smallest mammal, but it has the fastest heartbeat at 1,200 beats per minute.</h5>
+                                <h6>Fun facts- <span>Healthline.com</span></h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ application/views/shared/testimonials.php
@@ -0,0 +1,49 @@
+<section class="fancy-testimonials-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="testimonials-slides owl-carousel">
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                                <img src="<?php  echo base_url(); ?>assets/img/clients-img/1.jpg" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>I wanted to mention that these days, when the opposite of good customer and tech support tends to be the norm, it’s always great having a team like you guys at Fancy! So, be sure that I’ll always spread the word about how good your product is and the extraordinary level of support that you provide any time there is any need for it.</h5>
+                                <h6>Aigars Silkalns - <span>CEO DeerCreative</span></h6>
+                            </div>
+                        </div>
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                                <img src="<?php  echo base_url(); ?>assets/img/clients-img/1.jpg" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>I wanted to mention that these days, when the opposite of good customer and tech support tends to be the norm, it’s always great having a team like you guys at Fancy! So, be sure that I’ll always spread the word about how good your product is and the extraordinary level of support that you provide any time there is any need for it.</h5>
+                                <h6>Aigars Silkalns - <span>CEO DeerCreative</span></h6>
+                            </div>
+                        </div>
+                        <!-- Single Testimonial -->
+                        <div class="single-testimonial d-md-flex align-items-center">
+                            <!-- Testimonial Thumb -->
+                            <div class="testimonial-thumbnail">
+                                <img src="<?php  echo base_url(); ?>assets/img/clients-img/1.jpg" alt="">
+                            </div>
+                            <!-- Content -->
+                            <div class="testimonilas-content">
+                                <span class="playfair-font quote">“</span>
+                                <h5>I wanted to mention that these days, when the opposite of good customer and tech support tends to be the norm, it’s always great having a team like you guys at Fancy! So, be sure that I’ll always spread the word about how good your product is and the extraordinary level of support that you provide any time there is any need for it.</h5>
+                                <h6>Aigars Silkalns - <span>CEO DeerCreative</span></h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
