cb363fbc87616333dc3b82bea110e0291cf7ca4f
conrad96 <conrad.mugisha@gmail.com>
2020-01-17 11:53:11 +0300

    added ifarme with google maps to africourts building

--- application/views/contact.php
+++ application/views/contact.php
@@ -53,7 +53,9 @@
             <div class="row">
                 <div class="col-12">
                     <!-- Google Map -->
-                    <div id="googleMap"></div>
+                    <div id="google-map-canvas">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.751954252935!2d32.57527334258987!3d0.33047209123872845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbb737282783d%3A0xefb556ed7dc72747!2sAfri%20Courts%20Parking!5e0!3m2!1sen!2sug!4v1579243896051!5m2!1sen!2sug" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </div>
                 </div>
             </div>
 
