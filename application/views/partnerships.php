39ee27d76ea936491ded9ecbd5ac573ac3e26298
conrad96 <conrad.mugisha@gmail.com>
2020-01-18 12:02:50 +0300

    fixed css cards for partnerships team cards

--- application/views/partnerships.php
+++ application/views/partnerships.php
@@ -51,7 +51,7 @@
     <section class="single_blog_area section-padding-100" >
     <div class="row">
                 <div class="col-12 col-lg-12">
-                    <div class="row gutters">
+                    <div class="row no-gutters">
                         <!-- Single Post Share Info -->
                        <?php //$this->load->view("shared/social_media");  ?>
 
@@ -62,28 +62,28 @@
                                 
                                 <!-- Post Content -->
                                 <div class="post-content">                                    
-                                    <div class="row">
-                                        <div class="column">
+                                    <div class="row" id="partnership-cards-row">                                        
+                                        <div class="column-partnership">
                                             <div class="card">                                                			        				        	
                                                 <img src="<?php echo base_url(); ?>assets/img/pictures/partnerships/PHOTO-2018-12-04-08-58-29_4016x5000-resized.jpg" alt="" class="img-team">                                                                                                
-			        				        	<span class="team-title">Moses M. Kitakule  MBchB,FACP,FCCP</span>                                               
+                                                <span class="team-title">Moses M. Kitakule  MBchB,FACP,FCCP</span>                                               
                                                 <span class="job-title">Consultant Pulmonary, Sleep, Critical care Medicine.</span>
                                             </div>
-                                        </div>  
-                                        <div class="column">
+                                        </div> 
+                                        <div class="column-partnership">
                                             <div class="card">                                                			        				        	
                                                 <img src="<?php echo base_url(); ?>assets/img/pictures/partnerships/PHOTO-2018-12-06-01-16-00.jpg" alt="" class="img-team">                                                                                                
-			        				        	<span class="team-title">Emmy Okello  </span>                                               
+                                                <span class="team-title">Emmy Okello  </span>                                               
                                                 <span class="job-title">MD, PhD,  FACC.</span>
                                             </div>
-                                        </div>  
-                                        <div class="column">
+                                        </div> 
+                                        <div class="column-partnership">
                                             <div class="card">                                                			        				        	
                                                 <img src="<?php echo base_url(); ?>assets/img/pictures/partnerships/8ef531f8-0b48-4801-b2a3-b93665d5baa7_4016x5000.jpg" alt="" class="img-team">                                                                                                
-			        				        	<span class="team-title">Aliku Twalib </span>                                               
+                                                <span class="team-title">Aliku Twalib </span>                                               
                                                 <span class="job-title">MBChB, MMED, Consultant Paediatric Cardiologist </span>
                                             </div>
-                                        </div>                                                                                                                                                          
+                                        </div>                                                                                                                                                                                                                                                                                                                          
                                     </div>                                                                    
                                 </div>
                             </div>
