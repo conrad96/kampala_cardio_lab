0fce6348c62c9814fac6583faed07a9aa6d27845
conrad96 <conrad.mugisha@gmail.com>
2020-01-18 14:16:34 +0300

    added facebook posts from fb page

--- application/views/index.php
+++ application/views/index.php
@@ -1,6 +1,9 @@
 <?php $this->load->view("shared/header"); ?>
 
 <body>
+    <!-- facebook API -->
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=1572183289588771&autoLogAppEvents=1"></script>
     <!-- Preloader Start -->
     <div id="preloader">
         <div class="loader">
@@ -244,54 +247,7 @@
     <!-- ***** CTA Area End ***** -->
 
     <!-- ***** Blog Area Start ***** -->
-    <section class="fancy-blog-area section-padding-100-70">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading text-center">
-                        <h2>Latest News</h2>
-                        <p>We Are A Creative Digital Agency. Focused on Growing Brands Online</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <!-- Single Blog -->
-                <div class="col-12 col-md-4">
-                    <div class="single-blog-area wow fadeInUp" data-wow-delay="0.5s">
-                        <img src="<?php  echo base_url(); ?>assets/img/blog-img/blog-1.jpg" alt="">
-                        <div class="blog-content">
-                            <h5><a href="static-page.html">We Create Experiences</a></h5>
-                            <p>The Fancy that recognize the talent and effort of the best web designers, developers and agencies in the world.</p>
-                            <a href="static-page.html">Learn More</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog -->
-                <div class="col-12 col-md-4">
-                    <div class="single-blog-area wow fadeInUp" data-wow-delay="1s">
-                        <img src="<?php  echo base_url(); ?>assets/img/blog-img/blog-2.jpg" alt="">
-                        <div class="blog-content">
-                            <h5><a href="static-page.html">Simple, Fast And Fun</a></h5>
-                            <p>The Fancy that recognize the talent and effort of the best web designers, developers and agencies in the world.</p>
-                            <a href="static-page.html">Learn More</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Single Blog -->
-                <div class="col-12 col-md-4">
-                    <div class="single-blog-area wow fadeInUp" data-wow-delay="1.5s">
-                        <img src="<?php  echo base_url(); ?>assets/img/blog-img/blog-3.jpg" alt="">
-                        <div class="blog-content">
-                            <h5><a href="static-page.html">Device Friendly</a></h5>
-                            <p>The Fancy that recognize the talent and effort of the best web designers, developers and agencies in the world.</p>
-                            <a href="static-page.html">Learn More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php $this->load->view("shared/facebook_feed"); ?>
     <!-- ***** Blog Area End ***** -->
 
     <!-- ***** Footer Area Start ***** -->
---
+++ application/views/shared/facebook_feed.php
@@ -0,0 +1,76 @@
+<section class="fancy-blog-area section-padding-100-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center">
+                        <h2>Facebook Feed</h2>
+                        <!-- <p>We Are A Creative Digital Agency. Focused on Growing Brands Online</p> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Blog -->
+                <div class="col-12 col-md-6">
+                    <div class="single-blog-area wow fadeInUp" data-wow-delay="0.5s">                        
+                        <div class="blog-content">
+                                <!-- <h5><a href="static-page.html">We Create Experiences</a></h5>
+                                <p>The Fancy that recognize the talent and effort of the best web designers, developers and agencies in the world.</p>
+                                <a href="static-page.html">Learn More</a> -->
+                            <div class="fb-video" data-href="https://www.facebook.com/KampalaCardioLab/videos/450283355639885/" data-width="500" data-show-text="true"><blockquote cite="https://developers.facebook.com/KampalaCardioLab/videos/450283355639885/" class="fb-xfbml-parse-ignore">
+                                <a href="https://developers.facebook.com/KampalaCardioLab/videos/450283355639885/"></a><p>Happy New Year all!!
+                                A lot happened here at KCL (Kampala CardioLab) in 2019. Most significant was our move to a new location. We worked hard towards enhancing your overall experience at the practice in keeping with our commitment to excellent service built around you, the patient. 
+                                KCL is now located on Plot 7 Buganda Road, behind Wandegeya Police Station, on the 2nd Floor of Africourts Building.</p>Posted by 
+                                <a href="https://www.facebook.com/KampalaCardioLab/">Kampala CardioLab</a> on Tuesday, December 31, 2019</blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <div class="col-12 col-md-6">
+                    <div class="single-blog-area wow fadeInUp" data-wow-delay="0.5s">                        
+                        <div class="blog-content">                            
+                            <div class="fb-post" data-href="https://www.facebook.com/KampalaCardioLab/posts/2376845152566571" data-width="400" data-show-text="true">
+                                <blockquote cite="https://developers.facebook.com/KampalaCardioLab/posts/2376845152566571" class="fb-xfbml-parse-ignore">
+                                    <p>How to detect a heart attack early enough to do something about it.</p>Posted by 
+                                    <a href="https://www.facebook.com/KampalaCardioLab/">Kampala CardioLab</a> on&nbsp;
+                                    <a href="https://developers.facebook.com/KampalaCardioLab/posts/2376845152566571">Wednesday, December 4, 2019</a>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                                            
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="single-blog-area wow fadeInUp" data-wow-delay="0.5s">                        
+                        <div class="blog-content">                            
+                        <div class="fb-post" data-href="https://www.facebook.com/KampalaCardioLab/posts/2415465982037821" data-width="500" data-show-text="true">
+                            <blockquote cite="https://developers.facebook.com/KampalaCardioLab/posts/2415465982037821" class="fb-xfbml-parse-ignore">
+                                <p>Did you know?
+                                ❣️Heart disease represents a varied group of diseases    
+                                involving the heart and blood vessels.*...</p>Posted by 
+                                <a href="https://www.facebook.com/KampalaCardioLab/">Kampala CardioLab</a> on&nbsp;
+                                <a href="https://developers.facebook.com/KampalaCardioLab/posts/2415465982037821">Monday, January 13, 2020</a>
+                            </blockquote>
+                        </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-12 col-md-6">
+                    <div class="single-blog-area wow fadeInUp" data-wow-delay="0.5s">                        
+                        <div class="blog-content">                            
+                            <div class="fb-post" data-href="https://www.facebook.com/KampalaCardioLab/posts/2394220924162327" data-width="500" data-show-text="true">
+                                <blockquote cite="https://developers.facebook.com/KampalaCardioLab/posts/2394220924162327" class="fb-xfbml-parse-ignore">
+                                    <p>Kampala CardioLab wishes you a Merry X-Mas and a great 2020!
+                                    We will be closed 24/12/19 - 29/12/19 &amp; 01/01/2020.</p>
+                                    Posted by <a href="https://www.facebook.com/KampalaCardioLab/">Kampala CardioLab</a> on&nbsp;
+                                    <a href="https://developers.facebook.com/KampalaCardioLab/posts/2394220924162327">Monday, December 23, 2019</a>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </section>
