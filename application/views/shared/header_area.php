be5eeb81c7ce25886580f72fdc7c3332ad25e8dc
conrad96 <conrad.mugisha@gmail.com>
2020-02-14 14:10:51 +0300

    styled nav bar links and top header bg color

--- application/views/shared/header_area.php
+++ application/views/shared/header_area.php
@@ -1,4 +1,4 @@
-<header class="header_area" id="header">
+<header class="header_area" id="top-kcl-header">
         <div class="container-fluid h-100">
             <div class="row h-100">
                 <div class="col-12 h-100">
@@ -8,7 +8,7 @@
                         <div class="collapse navbar-collapse" id="fancyNav">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/Home/index">Home <span class="sr-only">(current)</span></a>
+                                    <a style="color: black;" class="nav-link" href="<?php echo base_url(); ?>index.php/Home/index">Home <span class="sr-only">(current)</span></a>
                                 </li>
                                 <!-- <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
@@ -22,7 +22,7 @@
                                     <a class="nav-link" href="<?php echo base_url(); ?>index.php/Home/about">About</a>
                                 </li> -->
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="<?php echo base_url(); ?>index.php/Home/about" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+                                    <a style="color: black;" class="nav-link dropdown-toggle" href="<?php echo base_url(); ?>index.php/Home/about" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/Home/about">Who We Are</a>                                        
                                     </div>
@@ -31,7 +31,7 @@
                                     <a class="nav-link" href="<?php echo base_url(); ?>index.php/Home/services">Services</a>
                                 </li> -->
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+                                    <a style="color: black;" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/Home/pages/cardiac_rehabilitation">Cardiac Rehabilitation</a>
                                         <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/Home/pages/diagnostic_echocardiography">Diagnostic Echocardiography</a>
@@ -43,14 +43,14 @@
                                     </div>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="<?php echo base_url(); ?>index.php/Home/team" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Our Team</a>
+                                    <a style="color: black;" class="nav-link dropdown-toggle" href="<?php echo base_url(); ?>index.php/Home/team" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Our Team</a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/Home/team">Our Team</a>                                        
                                         <a class="dropdown-item" href="<?php echo base_url(); ?>index.php/Home/partnerships">Partnerships</a>                                        
                                     </div>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/Home/contact">Contact Us</a>
+                                    <a style="color: black;" class="nav-link" href="<?php echo base_url(); ?>index.php/Home/contact">Contact Us</a>
                                 </li>                               
                             </ul>
                             <!-- Search & Shop Btn Area -->
@@ -63,4 +63,4 @@
                 </div>
             </div>
         </div>
-    </header>
+</header>
