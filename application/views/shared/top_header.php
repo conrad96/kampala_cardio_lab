a605a579c2f24d5207695ca7e83a0d9023e640ee
conrad96 <conrad.mugisha@gmail.com>
2020-03-10 08:20:00 +0300

    added slideshow to top header banner

--- application/views/shared/top_header.php
+++ application/views/shared/top_header.php
@@ -1,17 +1,20 @@
-<div class="fancy-hero-area bg-img bg-overlay animated-img" style="background-image: url(<?php echo base_url(); ?>assets/img/pictures/KCL-094-1024x684.jpg);">
+<div class="fancy-hero-area bg-img bg-overlay animated-img"  id="top-hero-banner">
+        <!-- style="background-image: url(<?php echo base_url(); ?>assets/img/pictures/KCL-094-1024x684.jpg);" -->
+        <video autoplay muted loop id="myVideo" style="width: 100%;height: 900px;">
+            <source src="<?php echo base_url(); ?>assets/clips/kcl_slideshow.mp4" type="video/mp4">
+        </video>
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-12">
                     <div class="fancy-hero-content text-center">
-                        <!-- Video Overview -->
-                        <div class="video-overview">
-                            <a href="<?php echo base_url(); ?>assets/clips/overview.mp4" class="video--play--btn"><i class="fa fa-play" aria-hidden="true"></i> Your heart is in our hands</a>
-                        </div>
-                        <h2></h2>
-                        <a href="<?php echo base_url(); ?>index.php/Home/about" class="btn fancy-btn fancy-active">About Us</a>
-                        <a href="#" id="make-appointment-btn" class="btn fancy-btn">Make appointment</a>
+                        <!-- Video Overview -->                                                
+                        <!-- <a href="<?php echo base_url(); ?>index.php/Home/about" class="btn fancy-btn fancy-active">About Us</a>
+                        <a href="#" id="make-appointment-btn" class="btn fancy-btn">Make appointment</a> -->
                     </div>
                 </div>
             </div>
         </div>
 </div>
+<script>
+    
+</script>
